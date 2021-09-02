fb7baf1486a1018b8666e5d750275c825bd67e79
Dmitrii Zwei <zweidmitr@gmail.com>
2021-09-02 19:05:30 +0300

    0. Что такое переменная. [#3610 #198130]

---
+++ src/ru/job4j/Variables.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332F;
+        boolean baby = true;
+    }
+}
