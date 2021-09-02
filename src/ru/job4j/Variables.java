f52088ac152b2fd9fd977231451a18cef1d412e5
Dmitrii Zwei <zweidmitr@gmail.com>
2021-09-02 19:28:02 +0300

    2. Переменные. [#172635 #198139]

--- src/ru/job4j/Variables.java
+++ src/ru/job4j/Variables.java
@@ -2,8 +2,18 @@ package ru.job4j;
 
 public class Variables {
     public static void main(String[] args) {
-        int age = 18;
-        float length = 78.0332F;
-        boolean baby = true;
+//        int age = 18;
+//        float length = 78.0332F;
+//        boolean baby = true;
+
+        int age = 30;
+        boolean male = true;
+        float height = 1.85F;
+        int numberOfBrick = 78965413;
+        long numberOfBacteria = 5416874615487L;
+        long distanceToSpaceObjects = 7987464646498794L;
+        boolean workPermit = true;
+        float fileSize = 4.74F;
+
     }
 }
