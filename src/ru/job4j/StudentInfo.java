ee7bf3ee2a6b8e8352971a84b9c4c57e8717fae5
Dmitrii Zwei <zweidmitr@gmail.com>
2021-09-02 17:16:19 +0300

    4.1. Разбор первой программы. [#159033 #197949]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmitrii");
+        System.out.println("09-05-2021");
+    }
+}
