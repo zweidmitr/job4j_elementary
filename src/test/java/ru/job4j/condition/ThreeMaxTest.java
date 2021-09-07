3b826cd694d7d72e3664452c82c2de629faa03bd
Dmitrii Zwei <zweidmitr@gmail.com>
2021-09-07 23:41:47 +0300

    4.5. Отладка программы в IDEA [#159464]

---
+++ src/test/java/ru/job4j/condition/ThreeMaxTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ThreeMaxTest {
+
+    @Test
+    public void firstMax() {
+        int first = 10;
+        int second = 5;
+        int third = 1;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 10;
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void secondMax() {
+        int first = 10;
+        int second = 50;
+        int third = 1;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 50;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void thirdMax() {
+        int first = 1;
+        int second = 5;
+        int third = 100;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 100;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void allEq() {
+        int first = 1;
+        int second = 1;
+        int third = 1;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void firstEqSecond() {
+        int first = 10;
+        int second = 10;
+        int third = 1;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 10;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void firstEqThrid() {
+        int first = 100;
+        int second = 1;
+        int third = 100;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 100;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void secondEqThrid() {
+        int first = 1;
+        int second = 100;
+        int third = 100;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 100;
+        Assert.assertEquals(expected, result);
+    }
+}
