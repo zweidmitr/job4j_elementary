7da824f160bcbc2cb203b663f39167f3dd1df472
Dmitrii Zwei <zweidmitr@gmail.com>
2021-09-12 15:59:53 +0300

    6. Date. Отображение даты. [#310017]

--- ch_02/src/main/java/ru/job4j/tracker/Item.java
+++ ch_02/src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,12 @@
 
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
 
@@ -16,6 +19,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getTime() {
+        return this.created;
     }
 
     public int getId() {
---
+++ ch_02/src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item it = new Item();
+        LocalDateTime tekTime = it.getTime();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentTime = tekTime.format(formatter);
+        System.out.println("На часах сейчас: " + currentTime);
+
+    }
+}
