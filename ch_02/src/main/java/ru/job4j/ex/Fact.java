14132a17af1a86f320a88d75f108cee5e2ba9e6d
Dmitrii Zwei <zweidmitr@gmail.com>
2021-09-15 23:10:27 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ ch_02/src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n < 0 it is bad");
+        }
+        int rsl = 1;
+        for (int index = 0; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
