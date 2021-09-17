d14b5a45ee6d56f0955a289272ff7beb26e08019
Dmitrii Zwei <zweidmitr@gmail.com>
2021-09-17 13:20:44 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

--- ch_02/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ ch_02/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 
 public class PhoneDictionaryTest {
@@ -15,5 +16,13 @@ public class PhoneDictionaryTest {
         phones.add(new Person("Petr", "Arsentev", "534872", "Bruansk"));
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenEmptyFind() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("ein", "zwei", "drei", "vier"));
+        ArrayList<Person> persons = phones.find("funf");
+        assertTrue(persons.isEmpty());
     }
 }
