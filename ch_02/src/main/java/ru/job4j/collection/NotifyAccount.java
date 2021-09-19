c6ced9a0db1037a148791cdb7f2e5aa9ef6f1d0c
Dmitrii Zwei <zweidmitr@gmail.com>
2021-09-19 12:21:55 +0300

    3. Модели данных и HashSet. [#218530 #204907]

--- ch_02/src/main/java/ru/job4j/collection/NotifyAccount.java
+++ ch_02/src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -6,9 +6,8 @@ import java.util.List;
 public class NotifyAccount {
     public static HashSet<Account> sent(List<Account> accounts) {
         HashSet<Account> rsl = new HashSet<>();
-        Account temp = new Account("test", "test", "test");
         for (Account acco : accounts) {
-                rsl.add(acco);
+            rsl.add(acco);
         }
         return rsl;
     }
