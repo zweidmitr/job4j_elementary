911dcff584e0dfcc5c8de4b41afd5446539816fe
Dmitrii Zwei <zweidmitr@gmail.com>
2021-10-05 15:49:30 +0300

    0. Stream API [#58166]

--- ch_02/src/main/java/ru/job4j/stream/StreamUsage.java
+++ ch_02/src/main/java/ru/job4j/stream/StreamUsage.java
@@ -66,6 +66,18 @@ public class StreamUsage {
         ).collect(Collectors.toList());
         bugsTwo.forEach(System.out::println);
         System.out.println("=======================");
+        System.out.println("=======================");
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(0);
+        numbers.add(-1);
+        numbers.add(2);
+        numbers.add(-3);
+        numbers.add(4);
+        numbers.add(-5);
+        List<Integer> filterNumbers = numbers.stream().filter(
+                elem -> elem > 0
+        ).collect(Collectors.toList());
+        filterNumbers.forEach(System.out::println);
 
     }
 }
