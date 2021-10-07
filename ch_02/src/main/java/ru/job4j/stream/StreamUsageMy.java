4a7a637ed5f3759ae6380771467cc4d0f5f49b12
Dmitrii Zwei <zweidmitr@gmail.com>
2021-10-07 10:15:11 +0300

    0. Stream API [#58166] correct

--- ch_02/src/main/java/ru/job4j/stream/StreamUsageMy.java
+++ ch_02/src/main/java/ru/job4j/stream/StreamUsageMy.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StreamUsage {
+public class StreamUsageMy {
     public static class Task {
         private final String name;
         private final long spent;
