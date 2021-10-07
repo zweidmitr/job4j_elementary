1987ca080bbb62785ccd8723d49ed777f345289a
Dmitrii Zwei <zweidmitr@gmail.com>
2021-10-07 12:27:43 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- ch_02/src/main/java/ru/job4j/stream/Profiles.java
+++ ch_02/src/main/java/ru/job4j/stream/Profiles.java
@@ -1,5 +1,7 @@
 package ru.job4j.stream;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,6 +9,8 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         List<Address> result = profiles.stream()
                 .map(Profile::getAddress)
+                .sorted((x, y) -> x.getCity().compareTo(y.getCity()))
+                .distinct()
                 .collect(Collectors.toList());
         result.forEach(System.out::println);
         return result;
--- ch_02/src/test/java/ru/job4j/stream/ProfilesTest.java
+++ ch_02/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -18,6 +18,28 @@ public class ProfilesTest {
         Profiles pr = new Profiles();
         String result = pr.collect(profilesList).get(1).getStreet();
         assertThat(result, is("neva"));
+    }
 
+    @Test
+    public void whenTwoDoubleElements() {
+        List<Profile> profilesList = new ArrayList<>();
+        profilesList.add(new Profile(new Address("msk", "red", 99, 2)));
+        profilesList.add(new Profile(new Address("spb", "neva", 25, 12)));
+        profilesList.add(new Profile(new Address("spb", "neva", 25, 12)));
+        Profiles pr = new Profiles();
+        int resultSize = pr.collect(profilesList).size();
+        assertThat(resultSize, is(2));
+    }
+
+    @Test
+    public void whenSortedElements() {
+        List<Profile> profilesList = new ArrayList<>();
+        profilesList.add(new Profile(new Address("msk", "red", 99, 2)));
+        profilesList.add(new Profile(new Address("spb", "neva", 25, 12)));
+        profilesList.add(new Profile(new Address("spb", "neva", 25, 12)));
+        profilesList.add(new Profile(new Address("msk", "blue", 99, 2)));
+        Profiles pr = new Profiles();
+        String result = pr.collect(profilesList).get(2).getStreet();
+        assertThat(result, is("neva"));
     }
 }
